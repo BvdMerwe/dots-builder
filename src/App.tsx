ef69f0112daa427220dba614e867d8f9c2f589b6
Bernard van der Merwe <barny.vdm@gmail.com>
2022-12-14 23:06:27 +0400

    add builder and assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,135 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import { useForm } from "react-hook-form";
 import './App.css';
 
+interface Layers {
+  name: string;
+  options: string[];
+  order?: number;
+}
+
 function App() {
+  
+  const [layers, setLayers] = useState<Layers[]>();
+  const [selectedLayers, setSelectedLayers] = useState<string[]>([]);
+  
+  useEffect(() => {
+    setLayers([
+      {
+        name: "Backgrounds",
+        options: [
+          "CHOCOLATE.png",
+          "DUSK.png",
+          "FERN.png",
+          "GRAPE.png",
+          "GRASS.png",
+          "LAVENDER.png",
+          "METAL.png",
+          "MINT.png",
+          "MUSTARD.png",
+          "RASBERRY.png",
+          "ROSE.png",
+          "SKY.png",
+          "TANGERINE.png",
+          "TOXIC.png",
+        ]
+      },
+      {
+        name: "Back",
+        options: [
+          "_0000s_0002_Inserted-Image.png",
+          "_0000s_0003_Back-Miner.png",
+          "_0000s_0004_Back-Gun.png",
+          "_0000s_0006_Inserted-Image.png",
+        ],
+        order: 0
+      },
+      {
+        name: "Clothing",
+        options: [
+          "_0000s_0009_From-selection.png",
+          "_0000s_0008_From-selection.png",
+          "_0000s_0007_Inserted-Image.png",
+          "_0000s_0005_From-selection.png",
+          "_0000s_0006_From-selection.png",
+          "_0000s_0004_From-selection.png",
+          "_0000s_0003_From-selection.png",
+          "_0000s_0002_From-selection.png",
+          "_0000s_0001_From-selection.png",
+        ]
+      },
+      {
+        name: "Eargear",
+        options: [
+        ]
+      },
+      {
+        name: "Eyewear",
+        options: [
+          "_0000s_0008_From-selection.png",
+          "_0000s_0007_From-selection.png",
+          "_0000s_0006_From-selection.png",
+          "_0000s_0004_From-selection.png",
+        ]
+      },
+      {
+        name: "Headgear",
+        options: [
+          "_0000s_0003_Inserted-Image.png",
+          "_0000s_0002_From-selection.png",
+          "_0000s_0000_From-selection.png",
+        ]
+      },
+      {
+        name: "Mouthgear",
+        options: []
+      },
+      {
+        name: "Paint",
+        options: []
+      },
+    ]);
+  }, [])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container m-auto">
+      <header className="rounded border mt-10 text-center">
+        <h1 className="font-black">DOTS Builder</h1>
       </header>
+      <main className="flex mb-10">
+        <div className="w-full w-1/2 p-3">
+          <form>
+          {layers?.map((layer, n) => (
+            <label key={n} className="w-full block">
+              <span className="">{layer.name}</span>
+              <select onChange={(e) => {
+                selectedLayers[n] = e.target.value;
+                setSelectedLayers([...selectedLayers])
+              }} className="w-full py-2 px-1 border rounded">
+                <option key={n} value=""></option>
+                {layer.options.map((image, n) => (
+                  <option key={n} value={`${layer.name}/${image}`}>{image.replace(".png", "")}</option>
+                ))}
+              </select>
+            </label>
+          ))}
+          </form>
+        </div>
+        <div className="w-full w-1/2 p-3">
+          <div className="p-3 relative">
+            {selectedLayers.map((url, n) => (
+              url && <img className="absolute inset-0 object-contain" style={{zIndex: n}} src={`${process.env.PUBLIC_URL}/images/${url}`} key={n}/>
+            ))}
+          </div>
+        </div>
+      </main>
+      <footer className="text-center !text-[12px]">
+        <p className="text-[12px]">
+          made with 🧠 by 
+          <a className="text-[12px]" href="https://twitter.com/bnardus">@bnardus.eth</a>&nbsp;&&nbsp;
+          <a className="text-[12px]" href="https://twitter.com/juicyjama_">@juicyjama.eth</a> 
+        </p>
+      </footer>
     </div>
   );
 }
